3af6b71ccda5be4e94ac37633db791d1d1f7ecee
akm <akm2000@gmail.com>
2016-08-04 18:31:02 +0900

    Fill posts migration

--- db/migrate/20160804092932_create_posts.rb
+++ db/migrate/20160804092932_create_posts.rb
@@ -1,11 +1,12 @@
+# coding: utf-8
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
-    create_table :posts do |t|
-      t.references :user, null: false, foreign_key: true
-      t.string :title
-      t.text :content
-      t.string :category
-      t.integer :rating
+    create_table :posts, comment: 'ポスト' do |t|
+      t.references :user, null: false, foreign_key: true, comment: 'ユーザ'
+      t.string :title   , null: false, comment: 'タイトル'
+      t.text :content                , comment: '内容'
+      t.string :category             , comment: 'カテゴリ'
+      t.integer :rating              , comment: 'レート'
 
       t.timestamps
     end

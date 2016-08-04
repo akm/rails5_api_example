1ea4d63195fc1d006ec64c61fd9929727d96597a
akm <akm2000@gmail.com>
2016-08-04 19:00:35 +0900

    bundle exec annotate -i

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)      not null
+#  password_digest :string(255)
+#  token           :string(255)
+#  description     :text(65535)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_token  (token) UNIQUE
+#
+
 class User < ApplicationRecord
   has_secure_token
   has_secure_password
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  title      :string(255)      not null
+#  content    :text(65535)
+#  category   :string(255)
+#  rating     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+
 FactoryGirl.define do
   factory :post do
     association :user, factory: :user

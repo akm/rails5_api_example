ecc628681f3a7f820ceb1aa27a8058f9e8ccc546
akm <akm2000@gmail.com>
2016-08-10 16:31:32 +0900

    Implement SessionsController

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,17 +2,30 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+    it "Creating new session with valid data should create new session" do
+      user = FactoryGirl.create(:user)
+      @request.headers["Content-Type"] = 'application/vnd.api+json'
+      post :create, params: {
+             data: {
+               type: 'sessions',
+               attributes: {
+                 name: user.name,
+                 password: 'password'
+               }
+             }
+           }
+      expect(response).to have_http_status(:created)
+      jdata = JSON.parse response.body
+      refute_equal user.token, jdata['data']['attributes']['token']
     end
   end
 
   describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+    it "Should delete session" do
+      user = FactoryGirl.create(:user)
+      delete :destroy, params: { id: user.token }
+      expect(response).to have_http_status(:no_content)
     end
   end
 

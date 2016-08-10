d49091733a0a7bc00ef51a517a2a5caec1d66657
akm <akm2000@gmail.com>
2016-08-10 11:39:46 +0900

    Set visibility to each private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,9 +38,8 @@ class UsersController < ApplicationController
     @user.destroy
   end
 
-  private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
+    private def set_user
       @user = User.find(params[:id])
     rescue ActiveRecord::RecordNotFound
       user = User.new
@@ -49,7 +48,7 @@ class UsersController < ApplicationController
     end
 
     # Only allow a trusted parameter "white list" through.
-    def user_params
+    private def user_params
       params.require(:user).permit(:name, :password_digest, :token, :description)
     end
 end

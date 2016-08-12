eea7f4d0f6fe6dbed1d11c06a7cad18b31056293
akm <akm2000@gmail.com>
2016-08-12 15:37:29 +0900

    Implement login

--- public/js/blog.js
+++ public/js/blog.js
@@ -3,12 +3,15 @@ $(function() {
     var App = {
         current_page: 1,
         template: null,
+        current_user_token: null,
+        current_user_name: null,
 
         init: function(){
             this.template = Hogan.compile($("#blogsArea").html());
             $(".linkToPrev").click(this.prevClick);
             $(".linkToNext").click(this.nextClick);
             $(".linkToLogin").click(this.loginClick);
+            $(".form-signin").submit(this.loginSubmit);
         },
 
         refresh: function(){
@@ -35,6 +38,30 @@ $(function() {
 
         loginClick: function(){
             App.showLoginDialog();
+        },
+
+        loginSubmit: function(){
+            var self = this;
+            App.current_user_name = $("#inputEmail").get(0).value;
+            var attrs = {
+                name: App.current_user_name,
+                password: $("#inputPassword").get(0).value
+            };
+            $.ajax("/sessions", {
+                method: 'POST',
+                contentType: 'application/vnd.api+json',
+                dataType: "json",
+                data: JSON.stringify({
+                    data: {
+                        type: 'sessions',
+                        attributes: attrs
+                    }
+                }),
+                processData: false
+            }).then(function(res) {
+                App.current_user_token = res.data.attributes.token;
+                $('.loginUserName').html(App.current_user_name);
+            });
         }
     };
 

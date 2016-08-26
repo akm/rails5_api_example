7de1f0fdc2f7619bae6cd741da070c1a7a689b0f
akm <akm2000@gmail.com>
2016-08-26 17:11:27 +0900

    Fix routing spec

--- spec/routing/v1/users_routing_spec.rb
+++ spec/routing/v1/users_routing_spec.rb
@@ -4,35 +4,27 @@ RSpec.describe V1::UsersController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/v1/users").to route_to("v1/users#index")
-    end
-
-    it "routes to #new" do
-      expect(:get => "/v1/users/new").to route_to("v1/users#new")
+      expect(:get => "/v1/users").to route_to("v1/users#index", format: :json)
     end
 
     it "routes to #show" do
-      expect(:get => "/v1/users/1").to route_to("v1/users#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/v1/users/1/edit").to route_to("v1/users#edit", :id => "1")
+      expect(:get => "/v1/users/1").to route_to("v1/users#show", :id => "1", format: :json)
     end
 
     it "routes to #create" do
-      expect(:post => "/v1/users").to route_to("v1/users#create")
+      expect(:post => "/v1/users").to route_to("v1/users#create", format: :json)
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/v1/users/1").to route_to("v1/users#update", :id => "1")
+      expect(:put => "/v1/users/1").to route_to("v1/users#update", :id => "1", format: :json)
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/v1/users/1").to route_to("v1/users#update", :id => "1")
+      expect(:patch => "/v1/users/1").to route_to("v1/users#update", :id => "1", format: :json)
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/v1/users/1").to route_to("v1/users#destroy", :id => "1")
+      expect(:delete => "/v1/users/1").to route_to("v1/users#destroy", :id => "1", format: :json)
     end
 
   end

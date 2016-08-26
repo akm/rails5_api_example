1fd491466483e28ab436fd7b2bed1f1d1ac4e25f
akm <akm2000@gmail.com>
2016-08-26 18:10:09 +0900

    Fix access_token

--- db/migrate/20160826070035_add_access_token_to_user.rb
+++ db/migrate/20160826070035_add_access_token_to_user.rb
@@ -1,5 +1,6 @@
 class AddAccessTokenToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :access_token, :string
+    add_column :users, :access_token, :string, null: false, after: 'email'
+    add_index :users, :access_token, unique: true
   end
 end

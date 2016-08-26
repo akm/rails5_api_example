a7fc53cd9dbb569222c61865144b4be78ccfd88f
akm <akm2000@gmail.com>
2016-08-26 18:14:56 +0900

    bundle exec annotate -i

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@
 #
 #  id                     :integer          not null, primary key
 #  email                  :string(255)      default(""), not null
+#  access_token           :string(255)      not null
 #  encrypted_password     :string(255)      default(""), not null
 #  reset_password_token   :string(255)
 #  reset_password_sent_at :datetime
@@ -15,10 +16,10 @@
 #  last_sign_in_ip        :string(255)
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
-#  access_token           :string(255)
 #
 # Indexes
 #
+#  index_users_on_access_token          (access_token) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #

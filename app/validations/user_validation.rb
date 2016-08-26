8144e78a5fefdf11cc14d7ff5d2d7527e25fcefe
akm <akm2000@gmail.com>
2016-08-26 18:14:36 +0900

    bin/rake validation:generate

--- app/validations/user_validation.rb
+++ app/validations/user_validation.rb
@@ -3,9 +3,11 @@ module UserValidation
 
   included do
     validates :email, presence: true
+    validates :access_token, presence: true
     validates :encrypted_password, presence: true
     validates :sign_in_count, presence: true, numericality: true
     validates_uniqueness_of :reset_password_token, allow_nil: true
+    validates_uniqueness_of :access_token
     validates_uniqueness_of :email
   end
 end
